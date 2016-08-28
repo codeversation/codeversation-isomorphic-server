bc2404ffe42b1561528c5fac4ede971ca44bad31
Alex Aralis <alex.aralis@gmail.com>
2016-08-28 18:42:05 +0000

    update deploy-script to stop if there is a failure

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git checkout -b gh-pages-prep
-npm run doc
-git add -f doc
-git commit -m 'commit the doc dir'
-git subtree split --prefix=doc -b gh-pages
-git push github gh-pages
-git checkout master
-git branch -D gh-pages-prep
+git checkout -b gh-pages-prep && \
+npm run doc && \
+git add -f doc && \
+git commit -m 'commit the doc dir' && \
+git subtree split --prefix=doc -b gh-pages && \
+git push github gh-pages && \
+git checkout master && \
+git branch -D gh-pages-prep && \
