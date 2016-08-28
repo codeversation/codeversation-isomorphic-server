e01a0cc35db1362da54eb238d9719b2b6072f3af
Alex Aralis <alex.aralis@gmail.com>
2016-08-28 16:29:41 +0000

    add documentation to utilities

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,9 @@
+/**
+ * general utilities for use in the client or server
+ * @module utilities
+ * @see module:utilities
+ */
+
 import { fromJS } from 'immutable';
 import mapValues from 'lodash/mapValues';
 import Promise from 'bluebird';
@@ -8,17 +14,39 @@ Promise.config({
 
 /**
  * Converts args into an uppercase, underscore seperated string.
- * @param {...*} Words
- * @returns {String} Result
+ * @function
+ * @param {string} words - variable number of string words to cat.
+ * @return {string} Result
  */
 export const constJoin = (...words) =>
   words.map(word => word.toUpperCase()).join('_');
 
+/**
+ * decode the plain json string into a store where
+ * all the objects are converted with immutable
+ * @function
+ * @param {string} json - the input json
+ * @return {object} store - the redux store with Immutable members
+ */
 export const decodeStore =
   json => mapValues(JSON.parse(json), val => fromJS(val));
 
+/**
+ * A simple logging function for debugging.
+ * Will not produce output in production.
+ * @function
+ * @param {string} messages - variable arg list of strings
+ */
 export const log = ::console.log;
 
+/**
+ * A promised delay function.  The promise returned can be canceled to
+ * stop the timer and prevent resolution.
+ * @function
+ * @param  {Number} duration - time in seconds to delay
+ * @return {Promise} delayPromise - promise that will resolve after
+ * `time` seconds
+ */
 export const pelay = time =>
   new Promise((resolve, reject, onCancel) => {
     const pelayTimeout = setTimeout(resolve, time*1000);
