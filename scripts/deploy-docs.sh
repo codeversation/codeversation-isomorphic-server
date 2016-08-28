c351b6334384aa380c0017c178a2542911961f1b
Alex Aralis <alex.aralis@gmail.com>
2016-08-28 18:38:20 +0000

    update deploy-docs script

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -3,7 +3,8 @@
 git checkout -b gh-pages-prep
 npm run doc
 git add -f doc
-git commit -m 'adding in the doc dir'
-git push +$(git subtree split --prefix=doc):gh-pages
-git checkout master 
+git commit -m 'commit the doc dir'
+git subtree split --prefix=doc -b gh-pages
+git push github gh-pages
+git checkout master
 git branch -D gh-pages-prep
