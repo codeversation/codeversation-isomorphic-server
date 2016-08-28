e5abfe6034eb29a64cd1cf505d96b375cc0a46ed
Alex Aralis <alex.aralis@gmail.com>
2016-08-28 18:47:34 +0000

    update deploy-docs

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -8,7 +8,7 @@ npm run doc && \
 git add -f doc && \
 git commit -m 'commit the doc dir' && \
 git subtree split --prefix=doc -b gh-pages && \
-git push github gh-pages
+git push github +gh-pages
 
 git checkout master && \
 git branch -D gh-pages-prep
