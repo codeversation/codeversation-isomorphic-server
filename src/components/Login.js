9999a6d9ea705f965f71503c05cad43ddf948138
jwlehman93 <jeremylehman9@gmail.com>
2016-10-04 18:24:15 -0400

    added form field group component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-
+import { FormControl, FormGroup } from 'react-bootstrap';
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      username:'',
+      password:''
+    }
+  }
+
+  handleUsernameChange(e) {
+    this.setState({username: e.target.value});
+  }
+
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <button>Login</button>
-          <button>Sign Up</button>
-        </form> 
-      </div>
+      <form>
+        <FormGroup>
+          <FormControl
+            type='email'
+            value={this.state.value}
+            placeholder='Enter username'
+            onChange={::this.handleUsernameChange}
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+      </form> 
     );
   }
 }
