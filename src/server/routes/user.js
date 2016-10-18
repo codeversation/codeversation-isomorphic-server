11e269261e035c7867a8dcb4a24a738578988b38
Alex Aralis <alex.aralis@gmail.com>
2016-10-18 14:12:12 -0400

    add delete function to user route.  the delete takes a user object with password and email and will delete that user if they exist.

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -30,4 +30,53 @@ router.post('/', (req, res) => {
   });
 });
 
+router.delete('/', (req, res) => {
+  try {
+    if(
+      !req.body ||
+      !req.body.user ||
+      !req.body.user.password ||
+      !req.body.user.email
+    ){
+      res.status(400).json({ message: 'Invalid request.'});
+      return;
+    }
+
+    let { password, email } = req.body.user;
+
+    User.find().then(users => log(users));
+    User.findOne({ email })
+      .then(user => {
+        if(user) {
+          log(user);
+          user.authenticate(password)
+            .then(() => {
+              user.remove()
+                .then(() => {
+                  log(user, 'deleted');
+                  res.json({ message: 'User deleted.', user });
+                })
+                .catch(err => {
+                  log(err);
+                  res.status(500).json({ message: 'User deletion failed.' });
+                });
+            })
+            .catch(err => {
+              log(err);
+              res.status(500).json({ message: 'Authentication failed.' });
+            });
+        } else {
+          res.status(404).json({ message: 'User not found' });
+        }
+      })
+      .catch(err => {
+        log(err);
+        res.status(404).json({ message: 'User not found' });
+      });
+    } catch (err) {
+      log(err);
+      res.status(500).json({ message: 'Server side exception.  User could not be deleted.'});
+    }
+});
+
 export default router;
