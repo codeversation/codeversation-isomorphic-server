b118282dd48dd50663b23cda10a928fa6d92ead1
jwlehman93 <jeremylehman9@gmail.com>
2016-10-26 15:14:53 -0400

    add error handling and textarea for output

--- src/components/Snippet.js
+++ src/components/Snippet.js
@@ -2,44 +2,20 @@ import React, { PropTypes, Component } from 'react'
 import { Button } from 'react-bootstrap';
 import {Grid, Row, Col } from 'react-bootstrap';
 import SnippetOutput from './SnippetOutput';
-import { COMPILER } from '../config';
-import { Opal } from 'opal-npm-wrapper';
 
 class Snippet extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      snippet: '',
-      snippetOutput: 'Click Run to see your Snippet Output'
+      snippet: 'return "hello'
     }
   }
-  handleRunSnippet() {
-    fetch(COMPILER + 'api/v1/ruby', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        code: this.state.snippet
-      })
-    })
-      .then(res => res.json())
-      .then((json) => {
-        const ret = eval(json.js);
-        this.setState({
-          snippetOutput:ret 
-        })
-      })
-      .catch(err => console.log(err));
-  }
+
   render() {
-    console.log(this.state.snippetOutput);
     return (
       <Grid>
         <h2> I am a snippet</h2>
-        <SnippetOutput output={this.state.snippetOutput}/>
-        <Button onClick={this.handleRunSnippet.bind(this)}>Run</Button>
+        <SnippetOutput snippet={this.state.snippet}/>
       </Grid>
     );
   }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const ENV = 'development';
 export const EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
-export const V1_API_BASE = '/api/v1';
+export const V1_API_BASE = 'api/v1';
 export const COMPILER='https://fathomless-ridge-25516.herokuapp.com/';
