6caf2776545018cce52a91d9c5caa7734ad98f2c
jwlehman93 <jeremylehman9@gmail.com>
2016-11-03 13:41:54 -0400

    component fetches code from db

--- src/components/Codeversation.js
+++ src/components/Codeversation.js
@@ -2,12 +2,40 @@ import React, { PropTypes, Component } from 'react'
 import PostTitle from './PostTitle';
 import Snippet from './Snippet';
 import CommentList from './CommentList';
+import Loading from './Loading';
 
 class Codeversation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      codeversation: {}
+    }
+  }
+  componentDidMount() {
+    fetch(`http://localhost:3000/api/v1/codeversation/${this.props.params.id}`, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({
+          isLoading: false,
+          codeversation: json
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <Loading />
+    }
     return (
       <div>
-        <PostTitle />                 
+        <PostTitle title={this.state.codeversation.title}/>                 
         <Snippet />
         <CommentList />
       </div> 
