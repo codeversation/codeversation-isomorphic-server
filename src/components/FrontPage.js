ba410b62e4b4f232dccea2b39363846ecf85d282
jwlehman93 <jeremylehman9@gmail.com>
2016-11-03 16:52:58 -0400

    add list group for post

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,9 +1,46 @@
 import React, { PropTypes, Component } from 'react';
+import Loading from './Loading';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+
 class FrontPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      codeversations: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/codeversation')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        this.state.codeversations = json;
+        this.setState({
+          isLoading: false
+        });
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <Loading />;
+    }
+    const { codeversations } = this.state;
     return (
-      <h1>
-      FrontPage</h1>
+      <div>
+        <h1>
+          FrontPage
+        </h1>
+        <ListGroup>
+            {codeversations.map((codeversation) => {
+              return (
+                <ListGroupItem header={codeversation.title}>author</ListGroupItem>
+              );
+            })}
+          </ListGroup>
+      </div>
     );
   }
 }
