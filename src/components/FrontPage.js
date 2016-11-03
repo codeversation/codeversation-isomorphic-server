91301d4e95d6c1daf91a59eb8bcb7ebe0a70e538
jwlehman93 <jeremylehman9@gmail.com>
2016-11-03 16:22:11 -0400

    add FrontPage

---
+++ src/components/FrontPage.js
@@ -0,0 +1,11 @@
+import React, { PropTypes, Component } from 'react';
+class FrontPage extends Component {
+  render() {
+    return (
+      <h1>
+      FrontPage</h1>
+    );
+  }
+}
+
+export default FrontPage;
