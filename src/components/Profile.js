9b9888afed6d58fc74354b619e211376e57c5993
Alex Aralis <alex.aralis@gmail.com>
2016-11-10 09:59:33 -0500

    user info is now listed on the /profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,27 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import Item from './Item';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Profile extends Component {
+	listUserInfo() {
+		let user = this.props.user;
+
+		return Object.keys(user)
+		.map(key =>
+			<ListGroupItem header={user[key]} key={key}>{key}</ListGroupItem>
+		);
+
+	}
+
   render() {
-    const user = this.props.user.toJS();
-    console.log(this.props);
+    const user = this.props.user;
     user.posts = ['hello', 'world'];
     return (
       <div>
+				<ListGroup>
+					{this.listUserInfo()}
+				</ListGroup>
         <h1>Recent Posts</h1>
         {user.posts.map((post, index) => <Item _text={post} key={index} />)}
       </div>
@@ -16,7 +29,7 @@ class Profile extends Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({user});
+const mapStateToProps = ({user}) => ({user: user.toJS()});
 
 const mapDispatchToProps = dispatch => ({});
 
