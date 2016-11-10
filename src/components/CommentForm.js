6ab229f52ef0aed18013b7482cc6c248ed31cb42
jwlehman93 <jeremylehman9@gmail.com>
2016-11-10 11:47:10 -0500

    comment adde dto list on post

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,10 +1,7 @@
 import React, { PropTypes, Component } from 'react'
 import FormFieldGroup from './FormFieldGroup';
 import { Grid, Row, Col, Button, Image } from 'react-bootstrap';
-import { connect } from 'react-redux';
 import { Link } from 'react-router';
-//redux
-import { user } from 'actions';
 
 class CommentForm extends Component {
   constructor(props) {
@@ -14,40 +11,7 @@ class CommentForm extends Component {
     };
   }
 
-  handleCommentChange(e) {
-    this.setState({comment: e.target.value});
-  }
 
-  handleComment(){
-    fetch('http://localhost:3000/api/v1/comment', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify( {
-        comment: {
-          _creator: this.props.user.toJS().id,
-          content: this.state.comment,
-          likes: 0,
-          _codeversation: this.props.codeversationId
-        }
-      })
-    })
-
-      .then((data) => {
-        this.setState({
-          comment: ''
-        });
-      })
-      .catch((err) => {
-        console.error(err)
-        alert('Error posting your comment.');
-      });
-
-    return;
-
-  }
 
   render() {
     const router = this.context.router;
@@ -59,8 +23,8 @@ class CommentForm extends Component {
               <FormFieldGroup
                 label='Post a Comment'
                 type='text'
-                value={this.state.comment}
-                onChange={this.handleCommentChange.bind(this)}
+                value={this.props.comment}
+                onChange={this.props.onCommentChange}
                 feedback={true}
               />
             </Col>
@@ -69,7 +33,7 @@ class CommentForm extends Component {
             <Col md={1}>
               <Button 
                 bsSize='large'
-                onClick={this.handleComment.bind(this)}
+                onClick={this.props.postComment}
               >
                 Post Comment
               </Button>
@@ -81,10 +45,4 @@ class CommentForm extends Component {
   }
 };
 
-const mapStateToProps = ({ user }) => ({user});
-const mapDispatchToProps = dispatch => ({});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CommentForm);
+export default CommentForm;
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,11 +1,15 @@
 import React, { PropTypes, Component } from 'react'
+import { connect } from 'react-redux';
 import PostComment from './PostComment';
 import CommentForm from './CommentForm';
+//redux
+import { user } from 'actions';
 
 class CommentList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      comment: '',
       comments: []
     }
   }
@@ -19,17 +23,79 @@ class CommentList extends Component {
       })
       .catch(err => console.error(err));
   }  
+
+  handleCommentChange(e) {
+    this.setState({comment: e.target.value});
+  }
+
+  handleComment(commentTxt){
+    fetch('http://localhost:3000/api/v1/comment', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify( {
+        comment: {
+          _creator: this.props.user.toJS().id,
+          content: this.state.comment,
+          likes: 0,
+          _codeversation: this.props.id
+        }
+      })
+    })
+      .then(data => data.json())
+      .then((json) => {
+        console.log(this.props);
+        console.log(json);
+        const newComment = [{
+          content: this.state.comment,
+          _creator: {
+            email: this.props.user.toJS().email
+          }
+        }];
+        this.setState({
+          comment: '',
+          comments: this.state.comments.concat(newComment)
+        });
+      })
+      .catch((err) => {
+        console.error(err)
+        alert('Error posting your comment.');
+      });
+
+    return;
+
+  }
+
   render() {
     return (
       <div>
         <h2>Comments</h2>
-        {this.state.comments.map((comment) => {
-            return <PostComment content={comment.content}/>;
+        {this.state.comments.map((comment, ind) => {
+          return (
+            <PostComment 
+              poster={comment._creator.email} 
+              content={comment.content}
+              key={ind}
+            />
+          );
         })}
-        <CommentForm codeversationId={this.props.id}/>
+        <CommentForm 
+          postComment={this.handleComment.bind(this)}
+          comment={this.state.comment}
+          codeversationId={this.props.id}
+          onCommentChange={this.handleCommentChange.bind(this)}
+        />
       </div> 
     );
   }
 };
 
-export default CommentList;
+const mapStateToProps = ({ user }) => ({user});
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CommentList);
