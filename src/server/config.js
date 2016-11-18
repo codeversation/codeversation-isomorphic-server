af23b36d30d3446c333f9dd3f0ab13c9205f9f5c
Alex Aralis <alex.aralis@gmail.com>
2016-11-18 18:17:30 -0500

    change SERVER_ROOT so it does not have build/

--- src/server/config.js
+++ src/server/config.js
@@ -4,7 +4,7 @@ dotenv.load();
 
 export const DB_URI = process.env.DB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const SERVER_ROOT = `${process.cwd()}/build`;
+export const SERVER_ROOT = `${process.cwd()}`;
 export const SALT_ROUNDS = 10;
 export const COMPILER_API_BASE = process.env.COMPILER_API_BASE;
 export const PORT = process.env.PORT || 3000;
