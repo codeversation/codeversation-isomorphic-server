a031a85a0c10be7d1d53e1e9cb918137e1ef86d5
Alex Aralis <alex.aralis@gmail.com>
2016-11-18 18:04:55 -0500

    pipe port number from environment or default to 3000

--- src/server/config.js
+++ src/server/config.js
@@ -7,3 +7,4 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const SERVER_ROOT = `${process.cwd()}/build`;
 export const SALT_ROUNDS = 10;
 export const COMPILER_API_BASE = process.env.COMPILER_API_BASE;
+export const PORT = process.env.PORT || 3000;
