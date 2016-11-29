cf50108a87c181267855e093bc33976351b1a0b4
Alex Aralis <alex.aralis@gmail.com>
2016-11-29 15:36:54 -0500

    good enough

--- src/components/PostHistory.js
+++ src/components/PostHistory.js
@@ -6,21 +6,50 @@ class PostHistory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      profileCodeversations: []
+      profileCodeversations: [],
+			fetching: false,
+			invalid: true,
     };
   }
 
+	componentWillReceiveProps(nextProps) {
+		let valid = nextProps.userId === this.props.userId;
+
+		if(valid){
+			this.setState({
+				invalid: false,
+			});
+		}
+	}
+
   componentDidMount() {
+		this.tryFetchData();
+  }
+
+	componentDidUpdate() {
+		this.tryFetchData();
+	}
+
+	tryFetchData() {
+		if(!this.state.fetching && this.state.invalid){
+			this.fetchData();
+		}
+	}
+
+
+
+	fetchData() {
     fetch('http://localhost:3000/api/v1/codeversation')
       .then(res => res.json())
       .then((codeversations) => {
         console.log(codeversations);
         this.setState({
-          profileCodeversations: codeversations.filter(codeversation => codeversation._creator.id === this.props.userId)
+          profileCodeversations: codeversations.filter(codeversation => codeversation._creator.id === this.props.userId),
+					fetching: false,
         })
       })
       .catch(err => console.error('Couldn\'t retrieve codeversations', err));
-  }
+	}
 
   render() {
     return (
