cf69677d43bdb8b1d45cf96bffa3396b0904c4f2
jwlehman93 <jeremylehman9@gmail.com>
2016-12-07 22:22:15 -0500

    codeversations listed from most recent to oldest

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -16,9 +16,10 @@ class FrontPage extends Component {
     fetch(`${ISO_ROOT}${V1_API_BASE}/codeversation`)
       .then((res) => res.json())
       .then((json) => {
-        this.state.codeversations = json;
         this.setState({
-          isLoading: false
+          isLoading: false,
+          codeversations: json.reverse()
+
         });
       })
       .catch((err) => console.error(err));
